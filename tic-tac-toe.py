6de328a3018ac3591284852366257f0ebc1b0bc2
tristan-badet <127488323+tristan-badet@users.noreply.github.com>
2023-03-21 10:02:14 +0100

    Descriptif terminé qui marche

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -8,7 +8,6 @@ Boutons = [[0,0,0],
            [0,0,0]
            ]
 
-
 #Joueurs
 
 Joueur_1 = "X"
@@ -21,58 +20,84 @@ Arret_de_partie = False
 #L'étape du jeu, par exemple premier round c'est joueur 1 qui joue
 
 rounds = 1
-
-#Affichage du gagnant
-
-Valeur_du_gagnant = ""
-
-def Affichage_gagnant():
-    if Valeur_du_gagnant == "X":
-                messagebox.showinfo("Tic-Tac-Toe", "Le Joueur 1 a gagné.")
-    elif Valeur_du_gagnant == "O":
-                messagebox.showinfo("Tic-Tac-Toe", "Le joueur 2 a gagné.")
-                
+          
 #Vérification de la grille 
 
-def verification():
+def verification_X():
     global Arret_de_partie
-    global Valeur_du_gagnant
-    for x in range(3):
-        if Boutons[x][0] == Boutons[x][1] == Boutons[x][2] and Boutons[x][0] != 0 and Boutons[x][1] != 0 and Boutons[x][2] != 0:
+    global Bouton_1,Bouton_2, Bouton_3, Bouton_4, Bouton_5,Bouton_6,Bouton_7,Bouton_8,Bouton_9
+    if Bouton_1["text"] == Bouton_2["text"] == Bouton_3["text"] == " X":
+          Arret_de_partie = True
+          messagebox.showinfo("Tic-Tac-Toe", "Le joueur 1 a gagné")    
+    elif Bouton_4["text"] == Bouton_5["text"] == Bouton_6["text"] == " X":
+          Arret_de_partie = True
+          messagebox.showinfo("Tic-Tac-Toe", "Le joueur 1 a gagné")
+    elif Bouton_7["text"] == Bouton_8["text"] == Bouton_9["text"] == " X":
+          Arret_de_partie = True
+          messagebox.showinfo("Tic-Tac-Toe", "Le joueur 1 a gagné")
+    elif Bouton_1["text"] == Bouton_4["text"] == Bouton_7["text"] == " X":
+          Arret_de_partie = True
+          messagebox.showinfo("Tic-Tac-Toe", "Le joueur 1 a gagné")
+    elif Bouton_2["text"] == Bouton_5["text"] == Bouton_8["text"] == " X":
+          Arret_de_partie = True
+          messagebox.showinfo("Tic-Tac-Toe", "Le joueur 1 a gagné")
+    elif Bouton_3["text"] == Bouton_6["text"] == Bouton_9["text"] == " X":
+          Arret_de_partie = True
+          messagebox.showinfo("Tic-Tac-Toe", "Le joueur 1 a gagné")
+    elif Bouton_1["text"] == Bouton_5["text"] == Bouton_9["text"] == " X":
             Arret_de_partie = True
-            Valeur_du_gagnant = Boutons[x][0]
-            Affichage_gagnant()
-            break
-        elif Boutons[0][x] == Boutons[1][x] == Boutons[2][x] and Boutons[0][x] != 0 and Boutons[1][x] != 0 and Boutons[2][x] != 0:
-               Arret_de_partie = True
-               Valeur_du_gagnant = Boutons[0][x]
-               Affichage_gagnant()
-               break
-        elif Boutons[0][0] == Boutons[1][1] == Boutons[2][2] and Boutons[0][0] != 0 and Boutons[1][1] != 0 and Boutons[2][2] != 0:
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 1 a gagné")   
+    elif Bouton_3["text"] == Bouton_5["text"] == Bouton_7["text"] == " X":
             Arret_de_partie = True
-            Valeur_du_gagnant = Boutons[0][0]
-            Affichage_gagnant()
-            break
-        elif Boutons[0][2] == Boutons[1][1] == Boutons[2][0] and Boutons[0][2] != 0 and Boutons[1][1] != 0 and Boutons[2][0] != 0:
-              Arret_de_partie = True
-              Valeur_du_gagnant = Boutons[0][2]
-              Affichage_gagnant
-              break
-        elif Boutons[0][0] != 0 and Boutons[0][1] != 0 and Boutons[0][2] != 0 and Boutons[1][0] != 0 and Boutons[1][1] != 0 and Boutons[1][2] != 0 and Boutons[2][0] != 0 and Boutons[2][1] != 0 and Boutons[2][2] != 0:
-              Arret_de_partie = True
-              messagebox.showinfo("Tic-Tac-Toe", "C'est un match nul entre le joueur 1 et le joueur 2.")
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 1 a gagné")
+    elif rounds == 10:
+            messagebox.showinfo("Tic-Tac-Toe", "Match nul entre les deux joueurs")
 
+def verification_O():
+      global Arret_de_partie
+      global Bouton_1,Bouton_2, Bouton_3, Bouton_4, Bouton_5,Bouton_6,Bouton_7,Bouton_8,Bouton_9
+      if Bouton_1["text"] == Bouton_2["text"] == Bouton_3["text"] == " O":
+            Arret_de_partie = True
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 2 a gagné")    
+      elif Bouton_4["text"] == Bouton_5["text"] == Bouton_6["text"] == " O":
+            Arret_de_partie = True
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 2 a gagné")
+      elif Bouton_7["text"] == Bouton_8["text"] == Bouton_9["text"] == " O":
+            Arret_de_partie = True
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 2 a gagné")
+      elif Bouton_1["text"] == Bouton_4["text"] == Bouton_7["text"] == " O":
+            Arret_de_partie = True
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 2 a gagné")
+      elif Bouton_2["text"] == Bouton_5["text"] == Bouton_8["text"] == " O":
+            Arret_de_partie = True
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 2 a gagné")
+      elif Bouton_3["text"] == Bouton_6["text"] == Bouton_9["text"] == " O":
+            Arret_de_partie = True
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 2 a gagné")
+      elif Bouton_1["text"] == Bouton_5["text"] == Bouton_9["text"] == " O":
+            Arret_de_partie = True
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 2 a gagné")   
+      elif Bouton_3["text"] == Bouton_5["text"] == Bouton_7["text"] == " O":
+            Arret_de_partie = True
+            messagebox.showinfo("Tic-Tac-Toe", "Le joueur 2 a gagné")
+      elif rounds == 10:
+            messagebox.showinfo("Tic-Tac-Toe", "Match nul entre les deux joueurs")
+
+    
+        
 def placement_de_signe(y):
     global rounds
     if rounds%2 != 0 and Arret_de_partie == False and y["text"] == " ":
         y["text"] += "X"
         rounds += 1
-        #A voir avec tkinter voir video widget
+        verification_X()
     elif rounds%2 == 0 and Arret_de_partie == False and y ["text"] == " ":
         y["text"] += "O"
         rounds += 1 
+        verification_O()
     else:
         messagebox.showerror("Tic-Tac-Toe", "La case est déjà utilisée")
+
 
 # Début de l'interface graphique
 
